f881cf384bb437b1bbc8681e90a450d116e14a79
xiaqiqi <xiaqiqi@outlook.com>
2026-06-18 00:43:41 +0800

    feat: add client build script, gitignore compiled JS

--- client/electron/preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('electronAPI', {
-    showNotification: (title, body) => {
-        ipcRenderer.send('show-notification', { title, body });
-    },
-    hideWindow: () => ipcRenderer.send('hide-window'),
-});
